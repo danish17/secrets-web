503fb8925cab95c492032514cace2a42f6d868ae
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-09 17:02:19 +0530

    feat: use arrow func

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,25 @@ import { Button, Flex, Heading, Spacer, Text } from "@chakra-ui/react";
 import { Main } from "../../components/layout/Main";
 import { Footer } from "../../components/layout/Footer";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default function Home() {
+const Home = () => {
+  const router = useRouter();
   return (
     <>
       <Head>
         <title>Secrets - Share n-time viewable secrets</title>
         <meta name="description" content="Share n-time viewable secrets" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Flex direction="column" flex="1" height="100vh">
+      <Flex direction="column" flex="1">
         <Main>
           <Flex alignItems="center" flexDir="column" gap={2}>
             <Image
               src="./logo.svg"
               alt="Secrets - Create and share n-time viewable secret messages."
-              height={200}
-              width={200}
+              height={150}
+              width={150}
             />
             <Heading textTransform="lowercase">Secrets</Heading>
             <Text textTransform="lowercase" textAlign="center">
@@ -34,9 +35,14 @@ export default function Home() {
                 md: "row",
               }}
             />
-            <Button rightIcon={<FiArrowRight />}>create secret</Button>
+            <Button
+              rightIcon={<FiArrowRight />}
+              onClick={() => router.push("/create")}
+            >
+              create secret
+            </Button>
             <Button variant="outline" rightIcon={<FiFileText />}>
-              api docs
+              docs
             </Button>
           </Flex>
         </Main>
@@ -44,4 +50,6 @@ export default function Home() {
       </Flex>
     </>
   );
-}
+};
+
+export default Home;
