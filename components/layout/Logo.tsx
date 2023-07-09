eab5231fe7e491904f29355abdc3a3bedaf921d6
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-09 23:01:53 +0530

    feat: variable logo for colormodes

---
+++ components/layout/Logo.tsx
@@ -0,0 +1,16 @@
+import { useColorMode } from "@chakra-ui/react";
+import Image, { ImageProps } from "next/image";
+
+const Logo = (props: Omit<ImageProps, "src">) => {
+  const { colorMode } = useColorMode();
+  const { alt, ...rest } = props;
+  return (
+    <Image
+      src={colorMode === "dark" ? "./logo-dark.svg" : "./logo.svg"}
+      alt={props.alt ?? ""}
+      {...rest}
+    />
+  );
+};
+
+export default Logo;
