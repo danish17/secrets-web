f19917d67340b722cd1d47224eb4d7ee9fc1c376
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-11 13:03:02 +0530

    fix: width

--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -12,7 +12,7 @@ export const Main = (props: FlexProps) => {
         alignItems="center"
         justifyContent="center"
         as={motion.div}
-        w={{ sm: "full", md: "66%", lg: "50%" }}
+        w={{ sm: "full", md: "50%", lg: "35%" }}
         initial={{
           opacity: 0,
         }}
