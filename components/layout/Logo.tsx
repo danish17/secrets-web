c5360828a3baa0404aa4b99832531f0f27b2a1c1
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-11 13:02:48 +0530

    feat: add link to logo

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,15 +1,18 @@
 import { useColorMode } from "@chakra-ui/react";
 import Image, { ImageProps } from "next/image";
+import Link from "next/link";
 
 const Logo = (props: Omit<ImageProps, "src">) => {
   const { colorMode } = useColorMode();
   const { alt, ...rest } = props;
   return (
-    <Image
-      src={colorMode === "dark" ? "/logo-dark.svg" : "/logo.svg"}
-      alt={props.alt ?? ""}
-      {...rest}
-    />
+    <Link href="/">
+      <Image
+        src={colorMode === "dark" ? "/logo-dark.svg" : "/logo.svg"}
+        alt={props.alt ?? ""}
+        {...rest}
+      />
+    </Link>
   );
 };
 
