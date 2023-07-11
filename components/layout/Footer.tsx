b96856438b75aa54f31fab1e4955d7fe58c38218
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-11 13:11:54 +0530

    feat: color mode toggle

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,40 +5,54 @@ import {
   Link,
   Stack,
   Text,
+  useColorMode,
 } from "@chakra-ui/react";
+import { BsMoonStarsFill, BsSunFill } from "react-icons/bs";
 import { FaGithub, FaLink, FaLinkedin } from "react-icons/fa";
 
-export const Footer = () => (
-  <Container as="footer" role="contentinfo" py={{ base: "12", md: "16" }}>
-    <Stack spacing={{ base: "4", md: "5" }} justify="center">
-      <Stack justify="center" direction="row" align="center">
-        <ButtonGroup variant="ghost">
-          <IconButton
-            as="a"
-            href="https://linkedin.com/in/danishshakeel"
-            aria-label="Website"
-            icon={<FaLink />}
-          />
-          <IconButton
-            as="a"
-            href="https://linkedin.com/in/danishshakeel"
-            aria-label="LinkedIn"
-            icon={<FaLinkedin />}
-          />
-          <IconButton
-            as="a"
-            href="https://github.com/secrets"
-            aria-label="GitHub"
-            icon={<FaGithub />}
-          />
-        </ButtonGroup>
+export const Footer = () => {
+  const { colorMode, setColorMode } = useColorMode();
+
+  return (
+    <Container as="footer" role="contentinfo" py={{ base: "12", md: "16" }}>
+      <Stack spacing={{ base: "4", md: "5" }} justify="center" align="center">
+        <Stack justify="center" direction="row" align="center">
+          <ButtonGroup variant="ghost">
+            <IconButton
+              as="a"
+              href="https://linkedin.com/in/danishshakeel"
+              aria-label="Website"
+              icon={<FaLink />}
+            />
+            <IconButton
+              as="a"
+              href="https://linkedin.com/in/danishshakeel"
+              aria-label="LinkedIn"
+              icon={<FaLinkedin />}
+            />
+            <IconButton
+              as="a"
+              href="https://github.com/secrets"
+              aria-label="GitHub"
+              icon={<FaGithub />}
+            />
+          </ButtonGroup>
+        </Stack>
+        <Text fontSize="sm" color="fg.subtle" textAlign="center">
+          created with 💛 by{" "}
+          <Link href="https://iamdani.sh" isExternal>
+            danish shakeel
+          </Link>
+        </Text>
+        <IconButton
+          aria-label={`Toggle ${colorMode} mode`}
+          icon={colorMode === "dark" ? <BsSunFill /> : <BsMoonStarsFill />}
+          w="fit-content"
+          onClick={() => {
+            colorMode === "dark" ? setColorMode("light") : setColorMode("dark");
+          }}
+        />
       </Stack>
-      <Text fontSize="sm" color="fg.subtle" textAlign="center">
-        created with 💛 by{" "}
-        <Link href="https://iamdani.sh" isExternal>
-          danish shakeel
-        </Link>
-      </Text>
-    </Stack>
-  </Container>
-);
+    </Container>
+  );
+};
