e84968496e074ee583868374cc625644803c8631
Danish Shakeel <danish.shakeel@rtcamp.com>
2023-07-12 18:11:09 +0530

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,28 @@ import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import theme from "../../theme";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GANALYTICS_ID}`}
+      />
+      <Script id="gtag-script" strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GANALYTICS_ID}', {
+                    page_path: window.location.pathname,
+                    });
+        `}
+      </Script>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
