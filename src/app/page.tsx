731a9906a0334c5f2af6fbe9b75e7e3efe5aab12
Danish Shakeel <danishshakeel54@gmail.com>
2024-03-01 00:03:41 +0530

    fix: remove spotlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import { Spotlight } from "@secrets/components/ui/Spotlight";
-
 export default function Home() {
-  return (
-    <div className="h-screen w-full flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
-      />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Secrets
-        </h1>
-        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-          Create private n-time viewable messages that can only be read by
-          people you choose.
-        </p>
-      </div>
-    </div>
-  );
+  return <p>Hello</p>;
 }
